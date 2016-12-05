d19dc29a6a96d2d972ec57e29f74a3588149b8b5
Eliott <eliott.neviere@gmail.com>
2016-12-05 18:37:20 +0100

    Sounds loading + Theme setup multithreading.

--- app/src/main/java/com/gw150914/jabberwocky/gui/MainActivity.java
+++ app/src/main/java/com/gw150914/jabberwocky/gui/MainActivity.java
@@ -27,6 +27,95 @@ public class MainActivity extends Activity implements View.OnClickListener,View.
     private TextView currentThemeTextView;
     private Sound soundAndreaPasLa, soundAucunRapport, soundDefection, soundFoutLaRage, soundGrosseBlague, soundHabile, soundHumour, soundLeGitan, soundMachiavellique, soundMagnerLeCul, soundMaitreMichel, soundMarcheBien, soundNoFuckingBalls, soundNouveaute, soundPasCool, soundPasDrole, soundPourquoi, soundPqReche, soundPrejudice, soundPtitZizi, soundPtiteBite, soundQueSePasseTIl, soundQuelqueSorte, soundQuoi, soundSante, soundScandaleux, soundSuperBaise, soundSuperSpirituel, soundTrahison, soundTropPlaisir, soundVieuxMan;
     private SoundEngine soundEngine;
+    private SoundPool soundPool;
+    Context appContext;
+
+    private class LoadThread1 extends Thread {
+
+        public void run() {
+            soundAndreaPasLa = new Sound("Andrea pas la",soundPool.load(appContext,R.raw.andrea_pas_la,1));
+            soundAucunRapport = new Sound("Aucun rapport",soundPool.load(appContext,R.raw.aucun_rapport,1));
+            soundDefection = new Sound("Defection",soundPool.load(appContext,R.raw.defection,1));
+            soundFoutLaRage = new Sound("Fout la rage",soundPool.load(appContext,R.raw.fout_la_rage,1));
+            soundGrosseBlague = new Sound("Grosse blague",soundPool.load(appContext,R.raw.grosse_blague,1));
+            soundHabile = new Sound("Habile",soundPool.load(appContext,R.raw.habile,1));
+            soundHumour = new Sound("Humour",soundPool.load(appContext,R.raw.humour,1));
+            soundLeGitan = new Sound("Le Gitan",soundPool.load(appContext,R.raw.le_gitan,1));
+            soundMachiavellique = new Sound("Machiavellique",soundPool.load(appContext,R.raw.machiavellique,1));
+            soundMagnerLeCul = new Sound("Magner le cul",soundPool.load(appContext,R.raw.magner_le_cul,1));
+
+            themeAll.addSound(soundAndreaPasLa);
+            themeAll.addSound(soundAucunRapport);
+            themeAll.addSound(soundDefection);
+            themeAll.addSound(soundFoutLaRage);
+            themeAll.addSound(soundGrosseBlague);
+            themeAll.addSound(soundHabile);
+            themeAll.addSound(soundHumour);
+            themeAll.addSound(soundLeGitan);
+            themeAll.addSound(soundMachiavellique);
+            themeAll.addSound(soundMagnerLeCul);
+
+            adapterAll.notifyDataSetChanged();
+        }
+    }
+    private class LoadThread2 extends Thread {
+
+        public void run() {
+            soundMaitreMichel = new Sound("Maitre Michel",soundPool.load(appContext,R.raw.maitre_michel,1));
+            soundMarcheBien = new Sound("Marche bien",soundPool.load(appContext,R.raw.marche_bien,1));
+            soundNoFuckingBalls = new Sound("No Fucking Balls",soundPool.load(appContext,R.raw.no_fucking_balls,1));
+            soundNouveaute = new Sound("Nouveaute",soundPool.load(appContext,R.raw.nouveaute,1));
+            soundPasCool = new Sound("Pas Cool",soundPool.load(appContext,R.raw.pas_cool,1));
+            soundPasDrole = new Sound("Pas Drole",soundPool.load(appContext,R.raw.pas_drole,1));
+            soundPourquoi = new Sound("Pourquoi",soundPool.load(appContext,R.raw.pourquoi,1));
+            soundPqReche = new Sound("PQ Reche",soundPool.load(appContext,R.raw.pq_reche,1));
+            soundPrejudice = new Sound("Prejudice",soundPool.load(appContext,R.raw.prejudice,1));
+            soundPtitZizi = new Sound("Ptit zizi",soundPool.load(appContext,R.raw.ptit_zizi,1));
+
+            themeAll.addSound(soundMaitreMichel);
+            themeAll.addSound(soundMarcheBien);
+            themeAll.addSound(soundNoFuckingBalls);
+            themeAll.addSound(soundNouveaute);
+            themeAll.addSound(soundPasCool);
+            themeAll.addSound(soundPasDrole);
+            themeAll.addSound(soundPourquoi);
+            themeAll.addSound(soundPqReche);
+            themeAll.addSound(soundPrejudice);
+            themeAll.addSound(soundPtitZizi);
+
+            adapterAll.notifyDataSetChanged();
+        }
+    }
+    private class LoadThread3 extends Thread {
+
+        public void run() {
+            soundPtiteBite = new Sound("Ptite bite",soundPool.load(appContext,R.raw.ptite_bite,1));
+            soundQueSePasseTIl = new Sound("Que se passe t'il ",soundPool.load(appContext,R.raw.que_ce_passe_t_il,1));
+            soundQuelqueSorte = new Sound("Quelque Sorte",soundPool.load(appContext,R.raw.quelque_sorte,1));
+            soundQuoi = new Sound("Quoi",soundPool.load(appContext,R.raw.quoi,1));
+            soundSante = new Sound("Sante",soundPool.load(appContext,R.raw.sante,1));
+            soundScandaleux = new Sound("Scandaleux",soundPool.load(appContext,R.raw.scandaleux,1));
+            soundSuperBaise = new Sound("Super Baise",soundPool.load(appContext,R.raw.super_baise,1));
+            soundSuperSpirituel = new Sound("Super Spirituel",soundPool.load(appContext,R.raw.super_spirituel,1));
+            soundTrahison = new Sound("Trahison",soundPool.load(appContext,R.raw.trahison,1));
+            soundTropPlaisir = new Sound("Trop plaisir",soundPool.load(appContext,R.raw.trop_plaisir,1));
+            soundVieuxMan = new Sound("Vieux man",soundPool.load(appContext,R.raw.vieux_man,1));
+
+            themeAll.addSound(soundPtiteBite);
+            themeAll.addSound(soundQueSePasseTIl);
+            themeAll.addSound(soundQuelqueSorte);
+            themeAll.addSound(soundQuoi);
+            themeAll.addSound(soundSante);
+            themeAll.addSound(soundScandaleux);
+            themeAll.addSound(soundSuperBaise);
+            themeAll.addSound(soundSuperSpirituel);
+            themeAll.addSound(soundTrahison);
+            themeAll.addSound(soundTropPlaisir);
+            themeAll.addSound(soundVieuxMan);
+
+            adapterAll.notifyDataSetChanged();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,12 +152,6 @@ public class MainActivity extends Activity implements View.OnClickListener,View.
         soundListDisplay.setOnItemClickListener(this);
         soundListDisplay.setOnItemLongClickListener(this);
 
-        /*
-         * Instantiate a sound pool for playing sounds later on.
-         * This pool should load all sounds in the future. (might require a scan)
-         *
-         * https://developer.android.com/reference/android/media/SoundPool.html
-         */
 
         //Instantiate the sound engine.
         soundEngine = new SoundEngine((AudioManager)this.getSystemService(Context.AUDIO_SERVICE));
@@ -77,10 +160,33 @@ public class MainActivity extends Activity implements View.OnClickListener,View.
         themeAll = new Theme("themeAll");
         themeFav = new Theme("themeFav");
 
-        //Load all embedded sounds in memory and create Sound objects
-        SoundPool soundPool = soundEngine.getSoundPool();
-        Context appContext = this.getApplicationContext();
+/*
+         * Create adapters for the ListView
+         * ListView will be fed from the soundNameList field of corresponding theme.
+         *
+         * https://developer.android.com/reference/android/widget/ListView.html#setAdapter%28android.widget.ListAdapter%29
+         */
+        adapterAll = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item,themeAll.getSoundNameList());
+        adapterFav = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, themeFav.getSoundNameList());
 
+        soundListDisplay.setAdapter(adapterAll);
+
+        currentTheme = themeAll;
+        currentThemeTextView.setText("Current theme is All");
+
+        //Load all embedded sounds in memory and create Sound objects
+        soundPool = soundEngine.getSoundPool();
+        appContext = this.getApplicationContext();
+
+        LoadThread1 loadingThread1 = new LoadThread1();
+        LoadThread2 loadingThread2 = new LoadThread2();
+        LoadThread3 loadingThread3 = new LoadThread3();
+
+        loadingThread1.run();
+        loadingThread2.run();
+        loadingThread3.run();
+
+        /*
         soundAndreaPasLa = new Sound("Andrea pas la",soundPool.load(appContext,R.raw.andrea_pas_la,1));
         soundAucunRapport = new Sound("Aucun rapport",soundPool.load(appContext,R.raw.aucun_rapport,1));
         soundDefection = new Sound("Defection",soundPool.load(appContext,R.raw.defection,1));
@@ -112,6 +218,7 @@ public class MainActivity extends Activity implements View.OnClickListener,View.
         soundTrahison = new Sound("Trahison",soundPool.load(appContext,R.raw.trahison,1));
         soundTropPlaisir = new Sound("Trop plaisir",soundPool.load(appContext,R.raw.trop_plaisir,1));
         soundVieuxMan = new Sound("Vieux man",soundPool.load(appContext,R.raw.vieux_man,1));
+        */
 
         //Below code is currently useless. Keep it for ref.
         /*
@@ -125,6 +232,7 @@ public class MainActivity extends Activity implements View.OnClickListener,View.
         */
 
         //Add created sounds in theme All
+        /*
         themeAll.addSound(soundAndreaPasLa);
         themeAll.addSound(soundAucunRapport);
         themeAll.addSound(soundDefection);
@@ -156,20 +264,7 @@ public class MainActivity extends Activity implements View.OnClickListener,View.
         themeAll.addSound(soundTrahison);
         themeAll.addSound(soundTropPlaisir);
         themeAll.addSound(soundVieuxMan);
-
-        /*
-         * Create adapters for the ListView
-         * ListView will be fed from the soundNameList field of corresponding theme.
-         *
-         * https://developer.android.com/reference/android/widget/ListView.html#setAdapter%28android.widget.ListAdapter%29
-         */
-        adapterAll = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item,themeAll.getSoundNameList());
-        adapterFav = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, themeFav.getSoundNameList());
-
-        soundListDisplay.setAdapter(adapterAll);
-
-        currentTheme = themeAll;
-        currentThemeTextView.setText("Current theme is All");
+        */
     }
 
     /*
