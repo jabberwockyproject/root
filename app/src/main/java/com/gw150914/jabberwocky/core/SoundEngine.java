5a08d99ed611681fd32d5cc012fa803e8dff9ca2
Eliott <eliott.neviere@gmail.com>
2016-12-09 14:42:31 +0100

    SoundEngine Comments & Stylizing

--- app/src/main/java/com/gw150914/jabberwocky/core/SoundEngine.java
+++ app/src/main/java/com/gw150914/jabberwocky/core/SoundEngine.java
@@ -3,26 +3,34 @@ package com.gw150914.jabberwocky.core;
 import android.media.AudioManager;
 import android.media.SoundPool;
 
+
+/*
+################################[ SoundEngine.class ]##################################
+# The SoundEngine is designed to play sounds with some specific settings and options. #
+#######################################################################################
+*/
+
 public class SoundEngine {
 
-    /******************
-     * PRIVATE FIELDS *
-     ******************/
+    /*****************************************************************************************
+     * ===================================[ PRIVATE FIELDS ]================================ *
+     *****************************************************************************************/
 
-    private int loop;
-    private int soundPriority;
-    private boolean hasCustomVolume;
-    private float customVolume;
-    private float maxVolume;
-    private float rate;
-    private SoundPool soundPool;
-    private AudioManager audioManager;
+    private int loop;                   //Should the sound loop? -1: infinite, 0: no, >0: specific loop
+    private int soundPriority;          //Sound priority
+    private boolean hasCustomVolume;    //Sound won't use the system/music sound volume if set at TRUE
+    private float customVolume;         //User defined volume
+    private float maxVolume;            //System maximum volume
+    private float rate;                 //Sound playback rate. 1: normal
+    private SoundPool soundPool;        //The SoundPool object managing playback and sound loading
+    private AudioManager audioManager;  //The System audio manager
 
 
-    /*******************
-     * PRIVATE CLASSES *
-     *******************/
+    /*****************************************************************************************
+     * ===================================[ NESTED CLASSES ]================================ *
+     *****************************************************************************************/
 
+    //Thread class whose goal is to play a sound identified by an ID
     private class PlaySound extends Thread {
         private int soundId;
 
@@ -34,9 +42,10 @@ public class SoundEngine {
         }
     }
 
-    /****************
-     * CONSTRUCTORS *
-     ****************/
+
+    /*****************************************************************************************
+     * ====================================[ CONSTRUCTORS ]================================= *
+     *****************************************************************************************/
 
     //Constructor 1
     public SoundEngine(AudioManager newAudioManager) {
@@ -49,54 +58,69 @@ public class SoundEngine {
     }
 
 
-    /*********************
-     * GETTORS / SETTORS *
-     *********************/
+    /*****************************************************************************************
+     * =================================[ GETTORS / SETTORS ]=============================== *
+     *****************************************************************************************/
 
+    //Get the soundPool system
+    public SoundPool getSoundPool() {
+        return soundPool;
+    }
+
+    //Set the loop value for sound playback
     public void setLoop(int newLoop) {
         loop = newLoop;
     }
 
+    //Set the priority value for sound playback
     public void setSoundPriority(int newPriority) {
         soundPriority = newPriority;
     }
 
+    //Set the custom volume value for sound playback
     public void setCustomVolume(float newVolume) {
         customVolume = newVolume;
         hasCustomVolume = true;
     }
 
+    //Set the rate value for sound playback
     public void setRate(float newRate) {
         rate = newRate;
     }
 
-    public SoundPool getSoundPool() {
-        return soundPool;
-    }
 
+    /*****************************************************************************************
+     * ==================================[ PUBLIC METHODS ]================================= *
+     *****************************************************************************************/
 
-    /******************
-     * PUBLIC METHODS *
-     ******************/
-
+    //Switch back to the system/music volume
     public void removeCustomVolume() {
         hasCustomVolume = false;
     }
 
+    //Get the current system/music volume
     private float getCurVolume() {
         return audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
     }
 
+    //Start a thread to play the sound "soundId"
     public void playSound(int soundId) {
         PlaySound playSoundThread = new PlaySound(soundId);
         playSoundThread.start();
     }
 
 
-    /*******************
-     * PRIVATE METHODS *
-     *******************/
+    /*****************************************************************************************
+     * =================================[ PRIVATE METHODS ]================================= *
+     *****************************************************************************************/
 
+    /****************[ getFinalSoundVolume ]*****************
+     * Return the final volume used to play sounds          *
+     * If the volume is custom, return the custom/max ratio *
+     * Else return the current/max ratio                    *
+     * Ratios are used because Max volume is sometimes      *
+     * not equal to 1 on some devices/situation             *
+     ********************************************************/
     private float getFinalSoundVolume() {
         if(hasCustomVolume) {
             return customVolume / maxVolume;
