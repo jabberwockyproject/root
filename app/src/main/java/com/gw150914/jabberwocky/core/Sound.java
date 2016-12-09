7b5271d9514794140d615e059c45ebc399224f00
Eliott <eliott.neviere@gmail.com>
2016-12-09 13:59:11 +0100

    Added Sound comments

--- app/src/main/java/com/gw150914/jabberwocky/core/Sound.java
+++ app/src/main/java/com/gw150914/jabberwocky/core/Sound.java
@@ -1,5 +1,14 @@
 package com.gw150914.jabberwocky.core;
 
+
+/*+---------[ Sound.class ]------------+
+  |The Sound class represents a sound. |
+  |Sounds Are loaded in memory and have|
+  |a name and a ID.                    |
+  |IDs are used by SoundPools to find  |
+  |sounds in memory                    |
+  +------------------------------------+*/
+
 public class Sound {
 
 
@@ -7,8 +16,8 @@ public class Sound {
      * PRIVATE FIELDS *
      ******************/
 
-    private String name;
-    private int soundId;
+    private String name;    //Sound name.
+    private int soundId;    //Sound ID. provided by a SoundPool. CHANGES AT EACH COLD-START
 
 
     /****************
@@ -38,18 +47,22 @@ public class Sound {
      * GETTORS / SETTORS *
      *********************/
 
+    //Return Sound name
     public String getName() {
         return name;
     }
 
+    //Return Sound ID
     public int getSoundId() {
         return soundId;
     }
 
+    //Set Sound name
     public void setName(String newName) {
         name = newName;
     }
 
+    //Set Sound ID
     public void setSoundID(int newSoundId) {
         soundId = newSoundId;
     }
