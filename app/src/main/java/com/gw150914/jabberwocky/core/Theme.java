5d79e92d8fb6e17bb7be7a0b9edfa0f7a2ec13b9
Eliott <eliott.neviere@gmail.com>
2016-12-09 17:03:47 +0100

    Added additional Theme comments

--- app/src/main/java/com/gw150914/jabberwocky/core/Theme.java
+++ app/src/main/java/com/gw150914/jabberwocky/core/Theme.java
@@ -59,32 +59,32 @@ public class Theme {
      * =================================[ GETTORS / SETTORS ]=============================== *
      *****************************************************************************************/
 
-    //Get the quantity of sounds in a theme
+    //Get the quantity of sounds in this theme
     public int getSoundsCount() {
         return soundsCount;
     }
 
-    //Get a theme's name
+    //Get this theme's name
     public String getName() {
         return name;
     }
 
-    //Get a theme's sound list.
+    //Get this theme's sound list.
     public Sound[] getSoundList() {
         return soundList;
     }
 
-    //Get a specific sound in a theme identified at "index" position
+    //Get a specific sound in this theme identified at "index" position
     public Sound getSound(int index) {
         return soundList[index];
     }
 
-    //Get the name of sounds in a theme.
+    //Get the name of sounds in this theme.
     public ArrayList<String> getSoundNameList() {
         return soundNameList;
     }
 
-    //Set the name of a theme
+    //Set the name of this theme
     public void setName(String newName) {
         name = newName;
     }
@@ -94,7 +94,7 @@ public class Theme {
      * ==================================[ PUBLIC METHODS ]================================= *
      *****************************************************************************************/
 
-    //Get a random sound contained in a theme
+    //Get a random sound contained in this theme
     public Sound getRandomSound() {
         Random random = new Random();
         return soundList[random.nextInt(soundsCount)];
@@ -112,17 +112,25 @@ public class Theme {
      * 3: Failed to add sound (other errors)                *
      ********************************************************/
     public int addSound(Sound newSound) {
+
+        //If the sound is not already present in this theme.
         if(!checkSoundDuplicate(newSound)) {
             try {
-                soundList[soundsCount] = newSound;
-                soundNameList.add(soundsCount,newSound.getName());
+                soundList[soundsCount] = newSound;                  //Add the new sound in soundList
+                soundNameList.add(soundsCount,newSound.getName());  //Add the new sound's name in soundNameList
             }
+
+            //Out of bound error
             catch(IndexOutOfBoundsException e) {
                 return 2;
             }
+
+            //Other errors
             catch(Exception e) {
                 return 3;
             }
+
+            //Increment soundsCount and return 0 (success)
             ++soundsCount;
             return 0;
         }
@@ -139,24 +147,41 @@ public class Theme {
      * True: Sound has been removed                         *
      ********************************************************/
     public boolean removeSound(Sound oldSound) {
-        boolean soundFound = false;
-        int index = 0;
+
+        boolean soundFound = false; //Has the sound been found?
+        int index = 0;              //Current working index
+
+        //Iterate through the soundList until the sound is found or all sounds are scanned.
         while(!soundFound && index < soundsCount) {
+
+            //If the sound is a match, set soundFound as true. Sounds' soundIds are used to find a match.
             if (oldSound.getSoundId() == soundList[index].getSoundId()) {
                 soundFound = true;
             }
+
+            //If sound sound isn't a match, increment the working index.
             else{
                 ++index;
             }
         }
+
+        //If the sound has been found.
         if(soundFound) {
+
+            //Remove it from soundNameList
             soundNameList.remove(index);
+
+            //Remove it from soundList and shift left all sounds from index + 1 to the soundList array's end.
             while(index < (soundsCount - 1)) {
                 soundList[index]=soundList[++index];
             }
+
+            //Decrement soundsCount and return true.
             --soundsCount;
             return true;
         }
+
+        //If the sound hasn't been found, return false.
         else {
             return false;
         }
@@ -167,6 +192,7 @@ public class Theme {
      * alphanumerical order                                 *
      ********************************************************/
     public void sortIndex() {
+
         boolean sorted = false;     //lists are sorted
         boolean hasDoneSomething;   //Something has been done during this pass
         int index;                  //Current working index
@@ -187,12 +213,12 @@ public class Theme {
                 if(soundNameList.get(index).compareToIgnoreCase(soundNameList.get(index + 1)) > 0) {
 
                     soundNameBuffer = soundNameList.get(index);             //Buffer soundNameList @index
-                    soundNameList.set(index, soundNameList.get(index + 1));   //Move soundNameList @index+1 to index
-                    soundNameList.set(index + 1,soundNameBuffer);             //Move buffered soundNameList to index+1
+                    soundNameList.set(index, soundNameList.get(index + 1)); //Move soundNameList @index+1 to index
+                    soundNameList.set(index + 1,soundNameBuffer);           //Move buffered soundNameList to index+1
 
                     soundBuffer = soundList[index];                         //Buffer soundList @index
-                    soundList[index] = soundList[index + 1];                  //Move soundList @index+1 to index
-                    soundList[index + 1] = soundBuffer;                       //Move buffered soundList to index+1
+                    soundList[index] = soundList[index + 1];                //Move soundList @index+1 to index
+                    soundList[index + 1] = soundBuffer;                     //Move buffered soundList to index+1
 
                     //Something has been done during this pass
                     hasDoneSomething = true;
@@ -213,8 +239,11 @@ public class Theme {
 
     //Check if soundList and soundNameList have a consistent index. Internal use only.
     private boolean checkIndexConsistency() {
-        boolean isConsistent = true;
-        int index = 0;
+
+        boolean isConsistent = true;    //Is the index consistent?
+        int index = 0;                  //Current working index.
+
+        //Iterate through both lists and check consistency. Break as soon as inconsistency is detected.
         while(isConsistent && index < soundsCount) {
             if(!(soundList[index].getName().equals(soundNameList.get(index)))) {
                 isConsistent = false;
@@ -225,8 +254,11 @@ public class Theme {
 
     //Check whether a sound is already in the soundList. Internal use only.
     private boolean checkSoundDuplicate(Sound soundToCheck) {
-        boolean duplicate = false;
-        int index = 0;
+
+        boolean duplicate = false;  //Is the sound a duplicate? (already present in this theme)
+        int index = 0;              //Current working index.
+
+        //Iterate through soundList and look for a match. Break and set duplicate at true as soon as a match is found.
         while(!duplicate && index < soundsCount) {
             if(soundToCheck.getSoundId() == soundList[index++].getSoundId()) {
                 duplicate = true;
