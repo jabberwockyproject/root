9235aceb172dc9c0bf10e0322fef703e30878671
Eliott <eliott.neviere@gmail.com>
2016-12-09 17:18:29 +0100

    Added additional ThemeEngine comments + Empty new method

--- app/src/main/java/com/gw150914/jabberwocky/core/ThemeEngine.java
+++ app/src/main/java/com/gw150914/jabberwocky/core/ThemeEngine.java
@@ -1,5 +1,13 @@
 package com.gw150914.jabberwocky.core;
 
+
+/*
+################################[ ThemeEngine.class ]##################################
+# The ThemeEngine class is designed to hold a collection of themes and various        #
+# methods to manipulate and check these stored themes.                                #
+#######################################################################################
+*/
+
 public class ThemeEngine {
 
 
@@ -7,10 +15,10 @@ public class ThemeEngine {
      * ===================================[ PRIVATE FIELDS ]================================ *
      *****************************************************************************************/
 
-    private static final int maxTheme = 30;
-    private int themeCount;
-    private Theme[] themeList;
-    private Theme currentTheme;
+    private static final int maxTheme = 30; //Maximum number of themes in a ThemeEngine. Final and static.
+    private int themeCount;                 //Number of themes contained in this ThemeEngine.
+    private Theme[] themeList;              //List of themes contained in this ThemeEngine.
+    private Theme currentTheme;             //Current active theme of this ThemeEngine (displayed theme)
 
 
     /*****************************************************************************************
@@ -28,22 +36,27 @@ public class ThemeEngine {
      * =================================[ GETTORS / SETTORS ]=============================== *
      *****************************************************************************************/
 
+    //Get the number of themes contained in this ThemeEngine.
     public int getThemeCount() {
         return themeCount;
     }
 
+    //Get the list of themes contained in this ThemeEngine.
     public Theme[] getThemeList() {
         return themeList;
     }
 
+    //Get the current active theme of this theme.
     public Theme getCurrentTheme() {
         return currentTheme;
     }
 
+    //Get a specific theme from themeList at the "index" position.
     public Theme getTheme(int index) {
         return themeList[index];
     }
 
+    //Set the current active theme of the ThemeEngine to "newTheme".
     public void setCurrentTheme(Theme newTheme) {
         currentTheme = newTheme;
     }
@@ -80,5 +93,10 @@ public class ThemeEngine {
         else {
             return false;
         }
+    }
+
+    public boolean removeTheme(Theme oldTheme){
+        //TODO
+        return false;
     }
 }
