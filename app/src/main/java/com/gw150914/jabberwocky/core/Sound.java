5aaf671947c538a3930575e900c251cccb69dcb0
Eliott <eliott.neviere@gmail.com>
2016-12-09 12:29:44 +0100

    Sound stylizing

--- app/src/main/java/com/gw150914/jabberwocky/core/Sound.java
+++ app/src/main/java/com/gw150914/jabberwocky/core/Sound.java
@@ -1,31 +1,55 @@
 package com.gw150914.jabberwocky.core;
 
 public class Sound {
+
+
+    /******************
+     * PRIVATE FIELDS *
+     ******************/
+
     private String name;
     private int soundId;
 
-    //TODO: Constructors
+
+    /****************
+     * CONSTRUCTORS *
+     ****************/
+
+    //Constructor 1
     public Sound() {
         name = "unnamed";
         soundId = 0;
     }
+
+    //Constructor 2
     public Sound(String newName) {
         name = newName;
         soundId = 0;
     }
+
+    //Constructor 3
     public Sound(String newName, int newSoundId) {
         name = newName;
         soundId = newSoundId;
     }
+
+
+    /*********************
+     * GETTORS / SETTORS *
+     *********************/
+
     public String getName() {
         return name;
     }
-    public void setName(String newName) {
-        name = newName;
-    }
+
     public int getSoundId() {
         return soundId;
     }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
     public void setSoundID(int newSoundId) {
         soundId = newSoundId;
     }
