f944f51824825c4ddff4e5e4dd33011448e2e469
Eliott <eliott.neviere@gmail.com>
2016-12-09 12:35:30 +0100

    SoundEngine stylizing

--- app/src/main/java/com/gw150914/jabberwocky/core/SoundEngine.java
+++ app/src/main/java/com/gw150914/jabberwocky/core/SoundEngine.java
@@ -4,14 +4,24 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 
 public class SoundEngine {
-    private SoundPool soundPool;
-    private float customVolume;
-    private float maxVolume;
-    private boolean hasCustomVolume;
+
+    /******************
+     * PRIVATE FIELDS *
+     ******************/
+
     private int loop;
     private int soundPriority;
+    private boolean hasCustomVolume;
+    private float customVolume;
+    private float maxVolume;
     private float rate;
+    private SoundPool soundPool;
     private AudioManager audioManager;
+
+
+    /*******************
+     * PRIVATE CLASSES *
+     *******************/
 
     private class PlaySound extends Thread {
         private int soundId;
@@ -24,6 +34,11 @@ public class SoundEngine {
         }
     }
 
+    /****************
+     * CONSTRUCTORS *
+     ****************/
+
+    //Constructor 1
     public SoundEngine(AudioManager newAudioManager) {
         soundPool = new SoundPool(20, AudioManager.STREAM_MUSIC,0);
         loop = 0;
@@ -32,38 +47,62 @@ public class SoundEngine {
         audioManager = newAudioManager;
         maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
     }
+
+
+    /*********************
+     * GETTORS / SETTORS *
+     *********************/
+
+    public void setLoop(int newLoop) {
+        loop = newLoop;
+    }
+
+    public void setSoundPriority(int newPriority) {
+        soundPriority = newPriority;
+    }
+
     public void setCustomVolume(float newVolume) {
         customVolume = newVolume;
         hasCustomVolume = true;
     }
+
     public void setRate(float newRate) {
         rate = newRate;
     }
-    public void setSoundPriority(int newPriority) {
-        soundPriority = newPriority;
-    }
-    public void setLoop(int newLoop) {
-        loop = newLoop;
-    }
+
     public SoundPool getSoundPool() {
         return soundPool;
     }
+
+
+    /******************
+     * PUBLIC METHODS *
+     ******************/
+
     public void removeCustomVolume() {
         hasCustomVolume = false;
     }
+
     private float getCurVolume() {
         return audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
     }
-    public void playSound(int soundId){
+
+    public void playSound(int soundId) {
         PlaySound playSoundThread = new PlaySound(soundId);
         playSoundThread.start();
     }
+
+
+    /*******************
+     * PRIVATE METHODS *
+     *******************/
+    
     private float getFinalSoundVolume() {
-        if (hasCustomVolume) {
-            return customVolume/maxVolume;
+        if(hasCustomVolume) {
+            return customVolume / maxVolume;
         }
         else {
-            return getCurVolume()/maxVolume;
+            return getCurVolume() / maxVolume;
         }
     }
 }
