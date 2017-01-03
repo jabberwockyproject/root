e4122b1f23a80c4b2f2e3e4858bfee06635ecdf3
Eliott <eliott.neviere@gmail.com>
2017-01-03 08:59:42 +0100

    Added empty InboundActivity. BSB is now listed as audio receiver.

---
+++ app/src/main/java/com/gw150914/jabberwocky/gui/InboundActivity.java
@@ -0,0 +1,15 @@
+package com.gw150914.jabberwocky.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.gw150914.jabberwocky.R;
+
+public class InboundActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbound);
+    }
+}
